a1b7a5765b38615267be7efd50deb7d8411689c7
MakikoOhashi <143975783+MakikoOhashi@users.noreply.github.com>
2023-11-16 14:17:33 +0900

    kadai_mdn_js upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,14 @@
+var time = new Date();
+
+  var year = time.getFullYear();
+  var month= time.getMonth()+1;
+  var day = time.getDate();
+  
+  console.log(year+'年'+month+'月'+day+'日');
+  
+//   console.log(month);
+//   console.log(day);
+
+//   console.log(time);
+
+  
